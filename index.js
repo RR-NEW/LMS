5f9075a5997bd373276f34f0b7abc1375f8fadf1
Rojit Rajbanshi <rojitrajbanshi69@gmail.com>
2026-07-17 15:41:50 +0545

    added index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+import dns from "dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 import express from "express";
-import mongoose from "mahgoose";
+import mongoose from "mongoose";
 import Users from "./database/models/users.js";
 
 const app = express();
 const PORT =1001;
 app.use(express.json());
 
-const mongoUri=""
+const mongoUri="mongodb+srv://<db_username>:qdMW2eBZUPiH90MD@lead.izrjkec.mongodb.net/";
 
 async function connectToDb()
 {
     try{
-        await mangoose.connect(mangoUri);
+        await mangoose.connect(mongoUri);
+        console.log("connected");
     }
     catch(error){
         console.log("Error in mongoose connection :\n", error);
@@ -21,6 +25,29 @@ async function connectToDb()
 connectToDb();
 app.get("/", (req, res)=>{
     return res.json({
-        name: ""
-    })
-})
+        name: "Rojit",
+    });
+});
+
+app.get("/users", async  (req, res) => {
+    const allUsers= await User.find();
+
+    return res.json({
+        status:"success",
+        users: allUsers,
+    });
+});
+app.post("/users",async (req, res)=> {
+    const formdata= req.body;
+    const newuser = await User.create(formData);
+
+    return res.json({
+        status: "success",
+        user: newuser,
+    
+
+    });
+});
+app.listen(PORT, () => {
+    console.log(`Serever is on port ${PORT}`);
+});
