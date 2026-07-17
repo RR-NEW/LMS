50b3590be1720e49fb8726cd765e320837188b0b
Rojit Rajbanshi <rojitrajbanshi69@gmail.com>
2026-07-17 13:42:52 +0545

    added functions

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 import express from "express";
 import mongoose from "mahgoose";
-import users from "./database/models/users.js";
+import Users from "./database/models/users.js";
 
+const app = express();
+const PORT =1001;
+app.use(express.json());
+
+const mongoUri=""
+
+async function connectToDb()
+{
+    try{
+        await mangoose.connect(mangoUri);
+    }
+    catch(error){
+        console.log("Error in mongoose connection :\n", error);
+    }
+}
+
+connectToDb();
+app.get("/", (req, res)=>{
+    return res.json({
+        name: ""
+    })
+})
